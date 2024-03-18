c36478b6f22d623bd3fc4329833cdec6f6361f24
ShivamBansal96 <bansal.shivam1216@gmail.com>
2024-03-18 21:17:39 +0530

    Day - 9

---
+++ src/screens/Homepage.jsx
@@ -0,0 +1,20 @@
+
+function Homepage() {
+    const style = {
+        display: 'flex',
+        justifyCenter: 'center',
+        alignItems: 'center'
+    }
+    
+    return(
+        <div id="homepage" style={style}>
+                {/* <h1>hello</h1> */}
+                <img src="https://1.bp.blogspot.com/-3JB5pjC3eqQ/XX30k-0oO_I/AAAAAAAAEyU/RBPwL6Z1pbIP2_w6aPF1fXd6bAxAaQY8wCLcBGAsYHQ/s1600/2-cute-shirtless-boy-abs-gym.jpg" alt="" />
+                <img src="https://th.bing.com/th/id/OIP.5uDRgWCGI-n6S7ZQjMUBjAHaHa?w=600&h=600&rs=1&pid=ImgDetMain" alt="" />
+                <img src="https://th.bing.com/th/id/OIP.zzjVsLx0DO6arfKeBaBr3wHaE8?rs=1&pid=ImgDetMain" alt="" />
+
+        </div>
+    )
+}
+
+export default Homepage;
